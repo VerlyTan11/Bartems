1e94806a1b772bd5e044563f7ce8655efc2ae62a
Nayla <naylamsb03@gmail.com>
2024-10-11 00:18:21 +0700

    add back to home button at DoneFragment.kt, add styles circle image di profile, add icon camera at add_item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation ("com.google.firebase:firebase-auth-ktx")
     implementation ("com.squareup.picasso:picasso:2.8")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.google.android.material:material:1.9.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/bartems/DoneFragment.kt
+++ app/src/main/java/com/example/bartems/DoneFragment.kt
@@ -1,16 +1,29 @@
 package com.example.bartems
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class DoneFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_done, container, false)
+        val view = inflater.inflate(R.layout.fragment_done, container, false)
+
+        // Handle button click to navigate to HomeActivity
+        val backToHomeButton = view.findViewById<Button>(R.id.button)
+        backToHomeButton.setOnClickListener {
+            val intent = Intent(activity, HomeActivity::class.java)
+            startActivity(intent)
+            activity?.finish() // Optionally close the current activity
+        }
+
+        return view
     }
 }
